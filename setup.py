9b81108d9c0e8d39487f3e1c0a19abe61b09c035
Laurent Gautier <lgautier@gmail.com>
2022-10-29 14:43:52 -0400

    Move build/install to PEP 517.

--- setup.py
+++ setup.py
@@ -1,23 +1,4 @@
-from distutils.core import setup
-
-pack_version = __import__('rpy2_R6').__version__
+import setuptools
 
 if __name__ == '__main__':
-    setup(
-        name='rpy2-R6',
-        version=pack_version,
-        description='Mapping the R package R6',
-        license='MIT',
-        requires=['rpy2(>=3.4)'],
-        packages=['rpy2_R6'],
-        classifiers=['Programming Language :: Python',
-                     'Programming Language :: Python :: 3',
-                     'Programming Language :: Python :: 3.6',
-                     'Programming Language :: Python :: 3.7',
-                     'Programming Language :: Python :: 3.8',
-                     ('License :: OSI Approved :: GNU General '
-                      'MIT'),
-                     'Intended Audience :: Developers',
-                     'Intended Audience :: Science/Research',
-                     'Development Status :: 3 - Alpha']
-    )
+    setuptools.setup()
